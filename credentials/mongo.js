f3c1b3688f5393a39ed49af5cebfd8967a751138
Rakhim111 <161516421+Rakhim111@users.noreply.github.com>
2024-03-02 05:51:30 +0600

    Update mongo.js

--- credentials/mongo.js
+++ credentials/mongo.js
@@ -1,4 +1,4 @@
 module.exports = {
-    remoteConnString: 'mongodb+srv://raddy:zIUeygMQsO3V7NAq@cluster0.gzm6dyd.mongodb.net/',
-    localConnString: 'mongodb://localhost:27017/todo'
+    remoteConnString: 'mongodb+srv://raddy:zIUeygMQsO3V7NAq@cluster0.gzm6dyd.mongodb.net/blog',
+    localConnString: 'mongodb+srv://raddy:zIUeygMQsO3V7NAq@cluster0.gzm6dyd.mongodb.net/blog'
 }
