c4a9c0b229ea0a2653921bc5ce17744adb4db465
Rakhim111 <161516421+Rakhim111@users.noreply.github.com>
2024-03-02 05:52:44 +0600

    Update todo.js

--- models/todo.js
+++ models/todo.js
@@ -9,37 +9,6 @@ const todoSchema = mongoose.Schema({
         type: String,
         required: true,
         default: 'N/A'
-    },
-    onDate: {
-        type: Date,
-        required: true,
-        default: Date.now
-    },
-    cardColor: {
-        type: String,
-        required: true,
-        default: '#cddc39'
-    },
-    isCompleted: {
-        type: Boolean,
-        required: true,
-        default: false
-    },
-    timestamps: {
-        createdOn: {
-            type: Date,
-            required: true,
-            default: Date.now
-        },
-        modifiedOn: {
-            type: Date,
-            required: true,
-            default: Date.now
-        },
-        completedOn: {
-            type: Date,
-            default: null
-        }
     }
 });
 
